ac12a531f5cd6005efd6c86bdf01a996242159ff
Stephanie Cheney <steph@scheneyphotography.com>
2020-09-30 21:14:44 -0500

    logo created and added

---
+++ front-end/src/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./styles.css"
+
+class HomePage extends React.Component{
+    state={}
+
+    render(){
+        return (
+        <>
+        <div>
+            <img width="18%" alt="" src={require('./images/BBLogo.png')}/>
+        </div>
+       
+        </>
+        )
+        
+    }
+}
+export default HomePage
--- front-end/src/Signup.js
+++ front-end/src/Signup.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react'
 import "./styles.css" 
+import HomePage from "./HomePage.js"
 
 class Signup extends Component{
     state={}
     render(){
         return(
-            <h1>Sign Up </h1>
+            <div class="parent">
+                <div class="div1"><HomePage/> </div>
+                <div class="div2"> This is Div 2</div>
+                <div class="div3"> This is Div 3</div>
+            </div>
+            
         ) 
     }
 }
