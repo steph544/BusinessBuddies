b01e0409a36fae4bbd4d77cfbf45fc1510958a55
steph544 <syang723@yahoo.com>
2020-10-12 06:47:58 -0500

    chatbot added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import './styles.css'
 import Signup from "./Signup.js"
 import 'semantic-ui-css/semantic.min.css'
+import ChatBot from 'react-simple-chatbot';
 
 class App extends React.Component {
   
--- front-end/src/MainWindow.js
+++ front-end/src/MainWindow.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import "./styles.css"
+import ChatBot from 'react-simple-chatbot';
 
 class MainWindow extends React.Component{
     state={}
 
-    render(){
+  
+      
+    render(){  
+        const steps = [
+        {
+          id: '0',
+          message: 'Welcome to react chatbot!',
+          trigger: '1',
+        },
+        {
+          id: '1',
+          message: 'Bye!',
+          end: true,
+        },
+      ];
         return (
         <>
         <div class="center">
             <img width="75%" alt="" src={require('./images/main.jpeg')}/>
+            <div>
+                <ChatBot steps={steps} />
+            </div>
         </div>
        
         </>
