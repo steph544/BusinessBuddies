c7575a136cb412748245df2d9a5bee85ed80b1b3
steph544 <syang723@yahoo.com>
2020-10-12 11:12:01 -0500

    fixing chatbot: git add . cd .. cd ..

--- front-end/src/MainWindow.js
+++ front-end/src/MainWindow.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import "./styles.css"
-import ChatBot from 'react-simple-chatbot';
 
 class MainWindow extends React.Component{
     state={}
@@ -8,24 +7,12 @@ class MainWindow extends React.Component{
   
       
     render(){  
-        const steps = [
-        {
-          id: '0',
-          message: 'Welcome to react chatbot!',
-          trigger: '1',
-        },
-        {
-          id: '1',
-          message: 'Bye!',
-          end: true,
-        },
-      ];
+
         return (
         <>
         <div class="center">
             <img width="75%" alt="" src={require('./images/main.jpeg')}/>
             <div>
-                <ChatBot steps={steps} />
             </div>
         </div>
        
