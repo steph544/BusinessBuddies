65f0e271c1a1e97b5d51fa8e599a53dc2a8e4edd
steph544 <syang723@yahoo.com>
2020-10-25 22:11:45 -0500

    added difficult people scenarios

--- front-end/src/DifficultPeople.js
+++ front-end/src/DifficultPeople.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./styles.css"
+import { Card, Icon, Button, Image } from 'semantic-ui-react'
 
 class DifficultPeople extends React.Component{
     state={
@@ -13,7 +14,46 @@ class DifficultPeople extends React.Component{
 
         return (
             <>   
-                <h1> Difficult People</h1>
+                 <Card.Group>
+                    <Card>
+                    <Card.Content>
+                    <Card.Header>Person 1</Card.Header>
+                        <Card.Description>
+                            This person is very upset that your product/service is not what they had expected. How do you solve this?
+                        </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                        <div className='ui two buttons'>
+                        <Button basic color='green'>
+                            Give a refund
+                        </Button>
+                        <Button basic color='red'>
+                            Explain why you cannot give a refund
+                        </Button>
+                        </div>
+                    </Card.Content>
+                    </Card>
+
+                    <Card>
+                    <Card.Content>
+                    <Card.Header>Person 2</Card.Header>
+                        <Card.Description>
+                            This person is asking you to do much more than what you originally offered in your service. How do you tell them you cannot do more without getting paid?
+                        </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                        <div className='ui two buttons'>
+                        <Button basic color='green'>
+                            Ask for payment first
+                        </Button>
+                        <Button basic color='red'>
+                            Tell them no.
+                        </Button>
+                        </div>
+                    </Card.Content>
+                    </Card>
+                
+                </Card.Group>
             </>  
         )
         
